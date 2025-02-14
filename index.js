af8669f3adfc572a9af8994f412bfb6eae80031e
LAB_C_REAL <labc@continental.edu.pe>
2025-02-13 22:10:25 -0500

    cambios en index.js

--- index.js
+++ index.js
@@ -7,21 +7,6 @@ const app = express();
 const port = 3000;
 const sql = neon(process.env.DATABASE_URL);
 
-// app.get('/', async (req, res) => {
-//     try {
-//         // Consultar los datos de la tabla Tasks
-//         const result = await sql`SELECT * FROM Tasks`;
-//         const tasks = result.rows; // Obtener las filas de la consulta
-        
-//         // Responder con los datos de las tareas
-//         res.json(tasks); // Puedes mostrar los datos en formato JSON
-
-//     } catch (error) {
-//         console.error('Error al obtener datos:', error);
-//         res.status(500).send('Hubo un error al obtener los datos');
-//     }
-// });
-
  // Ruta principal
 app.get('/', async (req, res) => {
 try {
